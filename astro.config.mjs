63e2b06555ea21e6b5b00476e11cb359a02296e4
Miguel Ángel Rodríguez <miguelangel.rodriguez.hernando@gmail.com>
2024-11-06 13:48:17 +0000

    Add prod build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,19 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
-import netlify from "@astrojs/netlify/functions";
 import react from "@astrojs/react";
 import partytown from "@astrojs/partytown";
 
+import node from "@astrojs/node";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind(),
-    react(),
-    partytown({
-      config: {
-        forward: ["dataLayer.push"],
-      },
-    }),
-  ],
+  integrations: [tailwind(), react(), partytown({
+    config: {
+      forward: ["dataLayer.push"]
+    }
+  })],
   output: "server",
-  adapter: netlify(),
+  adapter: node({
+    mode: "standalone"
+  })
 });
